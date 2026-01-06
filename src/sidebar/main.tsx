58116c623afce43381e1d5020c145cacdaf154d0
Nico Martin <mail@nico.dev>
2026-01-06 16:40:05 +0100

    removed env

--- src/sidebar/main.tsx
+++ src/sidebar/main.tsx
@@ -4,31 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const originalFetch = globalThis.fetch;
-globalThis.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
-  const url =
-    typeof input === "string"
-      ? input
-      : input instanceof URL
-        ? input.href
-        : input.url;
-
-  if (url.includes("huggingface.co")) {
-    const token = import.meta.env.VITE_HF_TOKEN;
-    if (token) {
-      const headers = new Headers(init?.headers);
-      headers.set("Authorization", `Bearer ${token}`);
-
-      init = {
-        ...init,
-        headers,
-      };
-    }
-  }
-
-  return originalFetch(input, init);
-};
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
